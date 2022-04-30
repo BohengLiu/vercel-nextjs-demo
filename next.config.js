a95b56341f64eb45304b13f896fff16308da7c67
robot-l <robot.l@binance.com>
2022-04-30 18:01:13 +0800

    feat: add wallet provider

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  infuraKey: process.env.INFURA_KEY,
+  alchemyKey: process.env.ALCHEMY_KEY,
+  magicKey: process.env.MAGIC_KEY,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
