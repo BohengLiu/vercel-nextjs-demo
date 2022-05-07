be73c74a683e439236d86b53e16306e6187d2c88
robot-l <robot.l@binance.com>
2022-05-07 09:59:57 +0800

    chore: add alias

--- next.config.js
+++ next.config.js
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   infuraKey: process.env.INFURA_KEY,
   alchemyKey: process.env.ALCHEMY_KEY,
   magicKey: process.env.MAGIC_KEY,
-};
+  webpack: (config, { isServer }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    }
 
-module.exports = nextConfig;
+    return config
+  },
+}
+
+module.exports = nextConfig
