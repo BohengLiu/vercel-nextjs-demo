c2a256fd3c3d7a71034bbef37cb4fc8b07136ecb
robot-l <robot.l@binance.com>
2022-05-07 10:00:23 +0800

    chore: change tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/uikit/**/*.{js,ts,jsx,tsx}',
   ],
   content: [],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
