10e8a95fd69d4a76034ce3acd6c1147d588dffd2
robot-l <robot.l@binance.com>
2022-07-11 17:58:59 +0800

    fix: add iframe

--- src/pages/bnc.tsx
+++ src/pages/bnc.tsx
@@ -1,19 +1,26 @@
-import React, { memo, useCallback, useEffect, useState } from 'react'
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react'
 import { NextPage } from 'next'
 
 const BNC: NextPage = () => {
+  const ref = useRef(null)
   const jumpToLogin = useCallback(() => {
     window.location.href = 'bnc://app.binance.com/setting/login'
     setTimeout(() => {
       window.location.href = '/'
     }, 500)
   }, [])
+  const handleLogin2 = useCallback(() => {
+    const el = document.createElement('iframe')
+    el.src = 'bnc://app.binance.com/setting/login'
+    ref.current.appendChild(el)
+  }, [])
   return (
-    <div className="flex flex-col h-screen">
+    <div ref={ref} className="flex flex-col h-screen">
       <main className="flex flex-col flex-grow">
         <h1>BNC Jump</h1>
         {/* <a href="bnc://app.binance.com/setting/login">login</a> */}
         <button onClick={jumpToLogin}>login btn</button>
+        <button onClick={handleLogin2}>login btn 2</button>
       </main>
     </div>
   )
